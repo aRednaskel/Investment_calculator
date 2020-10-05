a9475754eea3071f6de70d0570c3cd0c2450d6c0
Michał Nielipiński <nielipinskim@gmail.com>
2020-10-05 17:58:53 +0200

    fix: Spring can't find components without annotation so I added it

--- src/main/java/pl/fintech/challenge1/backend/controller/mapper/UserMapperImpl.java
+++ src/main/java/pl/fintech/challenge1/backend/controller/mapper/UserMapperImpl.java
@@ -1,8 +1,10 @@
 package pl.fintech.challenge1.backend.controller.mapper;
 
+import org.springframework.stereotype.Component;
 import pl.fintech.challenge1.backend.controller.dto.RegistrationDTO;
 import pl.fintech.challenge1.backend.domain.user.User;
 
+@Component
 public class UserMapperImpl implements UserMapper {
     @Override
     public User mapDTOToUser(RegistrationDTO registrationRequestDTO) {
