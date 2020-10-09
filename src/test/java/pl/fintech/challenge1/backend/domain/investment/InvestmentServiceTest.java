7c61fef8e33123895f22f27450d36584258090d3
Michał Nielipiński <nielipinskim@gmail.com>
2020-10-09 12:06:42 +0200

    Second test for first endpoint: checks if profits are correctly caluclated in simple case when there are no additional deposits

--- src/test/java/pl/fintech/challenge1/backend/domain/investment/InvestmentServiceTest.java
+++ src/test/java/pl/fintech/challenge1/backend/domain/investment/InvestmentServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -27,5 +29,20 @@ public class InvestmentServiceTest {
         assertEquals(0, graphData.getPayments().get(0));
         assertEquals(0, graphData.getProfit().get(0));
         assertEquals(0, graphData.getInvestmentValue().get(0));
+    }
+
+    @Test
+    public void shouldReturnCorrectValuesWithoutAdditionalContributions(){
+        InvestitionParams investitionParams = new InvestitionParams(1000L, 0L,
+                2L, 10L, DepositFrequency.MONTH);
+        GraphData graphData = investmentService.calculateInvestition(investitionParams);
+        //length after 2 years of each list should be 3
+        assertEquals(3, graphData.getPayments().size());
+        assertEquals(3, graphData.getProfit().size());
+        assertEquals(3, graphData.getInvestmentValue().size());
+
+        assertEquals(Arrays.asList(1000L, 2000L, 3000L), graphData.getPayments());
+        assertEquals(Arrays.asList(0d, 100d, 210d), graphData.getProfit().size());
+        assertEquals(Arrays.asList(1000d, 2100d, 3210d), graphData.getInvestmentValue().size());
     }
 }
