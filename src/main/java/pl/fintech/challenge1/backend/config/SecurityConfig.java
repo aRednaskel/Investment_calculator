5862e7369832a0a67a3b71d468b05dd1e5b787ce
Michał Nielipiński <nielipinskim@gmail.com>
2020-10-20 22:44:24 +0200

    cors should work finally

--- src/main/java/pl/fintech/challenge1/backend/config/SecurityConfig.java
+++ src/main/java/pl/fintech/challenge1/backend/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package pl.fintech.challenge1.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +13,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests()
+        http.cors().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+                .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .and()
                 .csrf().disable();
 
